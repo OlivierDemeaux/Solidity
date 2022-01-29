826a4e2aaa79ac0d890c58b17eae5125b7a4c63c
OlivierDemeaux <olivierdem218@gmail.com>
2022-01-29 08:53:58 -0800

    main contract moved

--- Interviews/DK/contracts/BettingContract.sol
+++ Interviews/DK/contracts/BettingContract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 contract Helpers {
+    event NewModerator(address newModerator);
 
     mapping (address => bool) moderators;
 
@@ -17,14 +18,25 @@ contract Helpers {
     function addModerator(address newModerator) public onlyMod {
         require(moderators[newModerator] == false, "Address already a moderator");
         moderators[newModerator] = true;
+        emit NewModerator(newModerator);
     }
 
     function isModerator(address moderator) public view returns(bool) {
         return(moderators[moderator]);
     }
+
+    function getTimestamp() public view returns(uint) {
+        return(block.timestamp);
+    }
 }
 
 contract BettingContract is Helpers {
+
+    event ContestCreated(uint256 contestId, uint256 startTime, uint256 endTime, uint256 maxBets, address moderator);
+    event ContestStarted(uint contestId);
+    event ContestEnded(uint contestId, bool homeWin);
+    event BetCreated(uint contestId, uint256 betAmount, bool homeWin);
+    event Payout(uint256 contestId, uint256 amountPaid, address userPaid);
 
     uint256 contestCounter;
 
@@ -50,7 +62,7 @@ contract BettingContract is Helpers {
         bool paid;
     }
 
-    mapping (uint => Contest) public contests;
+    mapping (uint => Contest) contests;
     mapping (address => mapping (uint => Bet)) existingBets;
 
     modifier onlyValidBetTime(uint256 contestId) {
@@ -62,17 +74,19 @@ contract BettingContract is Helpers {
         moderators[msg.sender] = true;
     }
 
-    function createContest(uint256 startTime, uint256 endTime, uint256 maxBets) public onlyMod() {
+    function createContest(uint256 startTime, uint256 endTime, uint256 maxBets) external onlyMod() {
         require(startTime > block.timestamp && startTime < endTime, "this contest timing is wrong");
         require(maxBets > 0, "maxBets cannot be 0");
         contests[contestCounter] = Contest(startTime, endTime, maxBets, 0, 0, 0, 0, 0, false, Status.NotStarted);
+        emit ContestCreated(contestId, startTime, endTime, maxBets, msg.sender);
+        contestCounter += 1;
     }
 
-    function bet(uint256 contestId, bool homeWin) public payable onlyValidBetTime(contestId) {
+    function bet(uint256 contestId, bool homeWin) external payable onlyValidBetTime(contestId) {
         require(contests[contestId].status == Status.Started, "Contest hasn't started or is already finished");
         require(contests[contestId].maxBets > contests[contestId].currentBets, "Max amount of bets reached for this contest");
         require(existingBets[msg.sender][contestId].exist == false, "You already have a existing bet for this contest");
-        require(msg.value > 0, "You need to bet some in order to win some");
+        require(msg.value > 100000000000000000, "Minimun bet is 0.1ETH");
 
         contests[contestId].currentBets += 1;
         existingBets[msg.sender][contestId].exist = true;
@@ -85,21 +99,24 @@ contract BettingContract is Helpers {
             contests[contestId].numberBetsAgainst += 1;
             contests[contestId].valueAgainst += msg.value;
         }
+        emit BetCreated(contestId, msg.value, homeWin);
     }
 
-    function beginContest(uint256 contestId) public onlyMod onlyValidBetTime(contestId) {
+    function beginContest(uint256 contestId) external onlyMod onlyValidBetTime(contestId) {
         require(contests[contestId].status == Status.NotStarted, "Contest already started");
         contests[contestId].status = Status.Started;
+        emit ContestStarted(contestId);
     }
 
-    function endContest(uint256 contestId, bool homeWin) public onlyMod {
+    function endContest(uint256 contestId, bool homeWin) external onlyMod {
         require(contests[contestId].endTime < block.timestamp, "Too early to close this contest");
         require(contests[contestId].status == Status.Started);
         contests[contestId].status = Status.Ended;
         contests[contestId].result = homeWin;
+        emit ContestEnded(contestId, homeWin);
     }
 
-    function getPayout(uint contestId) public  {
+    function getPayout(uint contestId) external  {
         require(contests[contestId].status == Status.Ended, "this contest has not ended yet");
         require(existingBets[msg.sender][contestId].exist == true, "you didn't bet on this contest");
         require(existingBets[msg.sender][contestId].homeWin == contests[contestId].result, "You didn't win this bet");
@@ -115,17 +132,18 @@ contract BettingContract is Helpers {
         uint totalPayoutToBetter = existingBets[msg.sender][contestId].betValue + betEarnings;
         existingBets[msg.sender][contestId].paid = true;
         payable(msg.sender).transfer(totalPayoutToBetter);
+        emit Payout(contestId, totalPayoutToBetter, msg.sender);
     }
 
-    function getTimestamp() public view returns(uint) {
-        return(block.timestamp);
-    }
-
-    function getBalance() public view returns(uint) {
+    function getBalance() external view returns(uint) {
         return(address(this).balance);
     }
 
-    function getContestStatus(uint contestId) public view returns(Status) {
+    function getContestStatus(uint contestId) external view returns(Status) {
         return(contests[contestId].status);
+    }
+
+    function getContestInfos(uint contestId) external view onlyValidBetTime(contestId) returns(Contest) {
+        return(contests[contestId]);
     }
 }
