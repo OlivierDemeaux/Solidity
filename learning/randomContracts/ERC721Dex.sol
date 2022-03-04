b68eab4899d6a8eefff44ed518ac4d89a6032575
OlivierDemeaux <olivierdem218@gmail.com>
2022-03-03 17:02:08 -0800

    modif erc721dex

--- learning/randomContracts/ERC721Dex.sol
+++ learning/randomContracts/ERC721Dex.sol
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 interface ERC721 {
-   //function balanceOf(address) external;
-    //function ownerOf(uint)external;
-    //function safeTransferFrom(address, address, uint)external;
-    //function transferFrom(address, address, uint) external;
-    //function approve(address, uint) external;
-    //function getApproved(uint) external;
-    //function setApprovalForAll(address, address) external;
-    //function isApprovedForAll(address, address) external;
-    function safeTransferFrom(address, address, uint, bytes calldata) external;
+
+    function safeTransferFrom(address, address, uint) external;
 }
 
 contract nftDex {
+
+    event NewOrder(address, address, uint, uint);
+    event tokenSold(address, address, uint, uint, address);
+    event orderCancelled(address, address, uint);
 
     uint orderCounter;
 
@@ -21,7 +18,7 @@ contract nftDex {
 
     struct Order {
         uint price;
-        uint tokenId;
+        uint tokenID;
         address tokenCollection;
         address creator;
     }
@@ -32,27 +29,32 @@ contract nftDex {
         owner = msg.sender;
     }
 
-    function createOrder(uint price, uint tokenId, address tokenCollection) external {
+    function createOrder(uint price, uint tokenID, address tokenCollection) external {
         require(price > 0 && tokenCollection != address(0));
-        ERC721(tokenCollection).safeTransferFrom(msg.sender, address(this), tokenId, "");
-        orders[orderCounter] = Order(price, tokenId, tokenCollection, msg.sender);
+        ERC721(tokenCollection).safeTransferFrom(msg.sender, address(this), tokenID);
+        orders[orderCounter] = Order(price, tokenID, tokenCollection, msg.sender);
         orderCounter += 1;
+        emit NewOrder(msg.sender, tokenCollection, tokenID, price);
     }
 
-    function acceptOrder(uint orderId) external payable{
-        Order memory curOrd = orders[orderId];
-        require(msg.sender != curOrd.creator, "Can't buy your own NFT");
-        require(curOrd.tokenCollection != address(0));
-        require(msg.value == curOrd.price, "Not the right price");
-        (bool success, ) = address(this).call{value: curOrd.price}("");
-        if (success) {
-            ERC721(curOrd.tokenCollection).safeTransferFrom(address(this), msg.sender, curOrd.tokenId, "");
-            orders[orderId].tokenCollection = address(0);
-        }
+    function buyToken(uint orderID) external payable{
+        Order memory order = orders[orderID];
+        require(msg.sender != order.creator, "Can't buy your own NFT");
+        require(order.tokenCollection != address(0));
+        require(msg.value == order.price, "Not the right price");
+        //sets the order price to 0 as a way to show the order was filled
+        orders[orderID].price = 0;
+        (bool success, ) = order.creator.call{value: order.price}("");
+        require(success, "Couldn't transfer ETH");
+        ERC721(order.tokenCollection).safeTransferFrom(address(this), msg.sender, order.tokenID);
+        emit tokenSold(msg.sender, order.tokenCollection, order.tokenID, order.price, order.creator);
     }
 
-    function cancelOrder(uint orderId) external {
-        require(msg.sender == orders[orderId].creator);
-        orders[orderId].tokenCollection = address(0);
+    function cancelOrder(uint orderID) external {
+        Order memory order = orders[orderID];
+        require(order.creator == msg.sender && order.price > 0);
+        orders[orderID].price = 0;
+        ERC721(order.tokenCollection).safeTransferFrom(address(this), order.creator, order.tokenID);
+        emit orderCancelled(msg.sender, order.tokenCollection, order.tokenID);
     }
 }
